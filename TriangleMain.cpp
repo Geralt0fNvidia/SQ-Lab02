a0a3da3f0e4ce9c7bf1c970b9aa2439db0b28a0e
NikProgramming <NikProgramming@users.noreply.github.com>
2020-09-26 14:50:16 -0400

    Update TriangleMain.cpp
    Updating main (do while, and "user" input)

--- TriangleMain.cpp
+++ TriangleMain.cpp
@@ -1,5 +1,4 @@
-#include"Triangle.h"
-
+#include "Triangle.h"
 
 
 
@@ -10,17 +9,26 @@ int main(void)
 	double angle2;
 	double side1;
 	double side2;
+	int change;
+
 	bool testCheck = false;
-    triangle userTriangle;
+	triangle userTriangle;
 
 	userTriangle.SideTest(side1);
 	userTriangle.SideTest(side2);
 	userTriangle.AngleTest(angle1);
 	userTriangle.AngleTest(angle2);
-	testCheck=userTriangle.TestingAngles(angle1, angle2);
+	testCheck = userTriangle.TestingAngles(angle1, angle2);
 	if (testCheck == false)
 	{
-		printf("Your two angles are equal to or above 180, Please Select what you want to change: /n 1.%f /n 2.%f");
+		do
+		{
+			printf("Your two angles are equal to or above 180, Please Select what you want to change: /n 1.%f /n 2.%f", angle1, angle2);
+			change = 1;
+			userTriangle.AngleTest(angle1);
+			testCheck = userTriangle.TestingAngles(angle1, angle2);
+		} while (testCheck != true);
+
 	}
 
 	return 0;
