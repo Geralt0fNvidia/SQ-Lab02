4191350f7bdacade925ba0fd1663cbb10c26dd1c
Justin200104 <57876808+Justin200104@users.noreply.github.com>
2020-09-26 14:12:03 -0400

    Add files via upload

---
+++ TriangleUnitTest/TriangleUnitTest.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab02/Triangle.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TriangleUnitTest
+{
+	TEST_CLASS(TriangleUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
