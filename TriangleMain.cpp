37a6245a09e996f95ee03ded8e8f16522ffb78f3
NikProgramming <NikProgramming@users.noreply.github.com>
2020-09-26 13:50:22 -0400

    Update TriangleMain.cpp
    Made some basic changes, as well as came up with class names for some of the userTriangle classes.

--- TriangleMain.cpp
+++ TriangleMain.cpp
@@ -10,12 +10,17 @@ int main(void)
 	double angle2;
 	double side1;
 	double side2;
+	bool testCheck = false;
     triangle userTriangle;
-	
-	if (angle1 + angle2 == 180)
+
+	userTriangle.SideTest(side1);
+	userTriangle.SideTest(side2);
+	userTriangle.AngleTest(angle1);
+	userTriangle.AngleTest(angle2);
+	testCheck=userTriangle.TestingAngles(angle1, angle2);
+	if (testCheck == false)
 	{
-
-
+		printf("Your two angles are equal to or above 180, Please Select what you want to change: /n 1.%f /n 2.%f");
 	}
 
 	return 0;
