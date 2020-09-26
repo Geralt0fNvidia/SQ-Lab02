7afc376b082576242a1ceea1f8b29e3ee3f951e0
NikProgramming <NikProgramming@users.noreply.github.com>
2020-09-26 13:37:40 -0400

    Update TriangleMain.cpp

--- TriangleMain.cpp
+++ TriangleMain.cpp
@@ -6,8 +6,17 @@
 
 int main(void)
 {
- triangle userTriangle;
- 
+	double angle1;
+	double angle2;
+	double side1;
+	double side2;
+    triangle userTriangle;
+	
+	if (angle1 + angle2 == 180)
+	{
+
+
+	}
 
 	return 0;
 
